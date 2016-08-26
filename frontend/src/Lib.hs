982c6b98b525339de86905afc272f87aac12518e
Ross MacLeod <rmm-github@z.odi.ac>
2016-08-26 00:47:24 -0400

    react!

--- frontend/src/Lib.hs
+++ frontend/src/Lib.hs
@@ -1,14 +1,50 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, TemplateHaskell #-}
 
 module Lib
-    ( console_log
-    , someFunc
-    ) where
+  ( someFunc
+  ) where
 
-import Data.JSString ()
-import GHCJS.Types
+import Control.Monad.Reader (ask)
+import Data.JSString (JSString)
+import Data.Maybe (fromMaybe)
+import GHC.Generics (Generic)
+import GHCJS.DOM (currentDocument)
+import GHCJS.DOM.Document (getBody, createElement)
+import GHCJS.DOM.Node (appendChild)
+import GHCJS.Marshal (FromJSVal, ToJSVal)
+import qualified React as R
+import qualified React.DOM as DOM
 
-foreign import javascript unsafe "console.log($1)" console_log :: JSString -> IO ()
+data Counter = Counter { dummy :: String, count :: Int } deriving (Generic)
+
+instance ToJSVal Counter
+instance FromJSVal Counter
+
+R.makeClass "counter"
+  [| R.statefulSpec (pure $ Counter "" 0) $ do
+      clicker <- R.eventHandler . const $ R.getState >>= R.setState . Counter "" . (+1) . count
+      pure $ do
+        count <- count <$> R.getState
+        this <- ask
+        pure $
+          DOM.div_ []
+            [ DOM.div_ [] ["The count is ", R.text $ show count]
+            , DOM.button_ [R.onClick this clicker] ["Click!"]
+            ]
+  |]
+counter :: R.ReactClass R.OnlyAttributes
+
+root :: R.ReactElement
+root =
+  DOM.div_ []
+    [ DOM.h1_ [] ["hello world!"]
+    , R.createElement counter [] []
+    ]
 
 someFunc :: IO ()
-someFunc = console_log "Hello from GHCJS!"
+someFunc = do
+  doc <- fromMaybe (error "no current document!") <$> currentDocument
+  body <- fromMaybe (error "no body!") <$> getBody doc
+  mountpoint <- fromMaybe (error "couldn't create a div!") <$> createElement doc (Just ("div" :: JSString))
+  _ <- appendChild body (Just mountpoint)
+  R.render root mountpoint Nothing
